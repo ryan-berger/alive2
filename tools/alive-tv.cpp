99c846933d890f4aa4bb9d94ad6f7b7a2e311408
Ryan Berger <ryanbberger@gmail.com>
2022-04-19 09:03:37 -0600

    add ANDS instruction

--- tools/alive-tv.cpp
+++ tools/alive-tv.cpp
@@ -710,30 +710,33 @@ IR::Value *cur_n{nullptr};
 IR::Value *cur_c{nullptr};
 
 set<int> s_flag = {
-    AArch64::ADDSWrs, AArch64::ADDSWri, AArch64::ADDSXrs, AArch64::ADDSXri,
-    AArch64::SUBSWrs, AArch64::SUBSWri, AArch64::SUBSXrs, AArch64::SUBSXri,
+    AArch64::ADDSWrs, AArch64::ADDSWri, AArch64::ADDSXrs,
+    AArch64::ADDSXri, AArch64::SUBSWrs, AArch64::SUBSWri,
+    AArch64::SUBSXrs, AArch64::SUBSXri, AArch64::ANDSWri,
 };
 
 set<int> instrs_32 = {
-    AArch64::ADDWrx,   AArch64::ADDSWrs, AArch64::ADDSWri, AArch64::ADDWrs,
-    AArch64::ADDWri,   AArch64::ASRVWr,  AArch64::SUBWri,  AArch64::SUBWrs,
-    AArch64::SUBWrx,   AArch64::SUBSWrs, AArch64::SUBSWri, AArch64::SBFMWri,
-    AArch64::CSELWr,   AArch64::ANDWri,  AArch64::ANDWrr,  AArch64::MADDWrrr,
-    AArch64::MSUBWrrr, AArch64::EORWri,  AArch64::CSINVWr, AArch64::CSINCWr,
-    AArch64::MOVZWi,   AArch64::MOVNWi,  AArch64::MOVKWi,  AArch64::LSLVWr,
-    AArch64::LSRVWr,   AArch64::ORNWrs,  AArch64::UBFMWri, AArch64::BFMWri,
-    AArch64::ORRWrs,   AArch64::ORRWri,  AArch64::SDIVWr,  AArch64::UDIVWr,
+    AArch64::ADDWrx,   AArch64::ADDSWrs,  AArch64::ADDSWri, AArch64::ADDWrs,
+    AArch64::ADDWri,   AArch64::ANDSWri,  AArch64::ASRVWr,  AArch64::SUBWri,
+    AArch64::SUBWrs,   AArch64::SUBWrx,   AArch64::SUBSWrs, AArch64::SUBSWri,
+    AArch64::SBFMWri,  AArch64::CSELWr,   AArch64::ANDWri,  AArch64::ANDWrr,
+    AArch64::MADDWrrr, AArch64::MSUBWrrr, AArch64::EORWri,  AArch64::CSINVWr,
+    AArch64::CSINCWr,  AArch64::MOVZWi,   AArch64::MOVNWi,  AArch64::MOVKWi,
+    AArch64::LSLVWr,   AArch64::LSRVWr,   AArch64::ORNWrs,  AArch64::UBFMWri,
+    AArch64::BFMWri,   AArch64::ORRWrs,   AArch64::ORRWri,  AArch64::SDIVWr,
+    AArch64::UDIVWr,
 };
 
 set<int> instrs_64 = {
-    AArch64::ADDXrx,   AArch64::ADDSXrs, AArch64::ADDSXri, AArch64::ADDXrs,
-    AArch64::ADDXri,   AArch64::ASRVXr,  AArch64::SUBXri,  AArch64::SUBXrs,
-    AArch64::SUBXrx,   AArch64::SUBSXrs, AArch64::SUBSXri, AArch64::SBFMXri,
-    AArch64::CSELXr,   AArch64::ANDXri,  AArch64::ANDXrr,  AArch64::MADDXrrr,
-    AArch64::MSUBXrrr, AArch64::EORXri,  AArch64::CSINVXr, AArch64::CSINCXr,
-    AArch64::MOVZXi,   AArch64::MOVNXi,  AArch64::MOVKXi,  AArch64::LSLVXr,
-    AArch64::LSRVXr,   AArch64::ORNXrs,  AArch64::UBFMXri, AArch64::BFMXri,
-    AArch64::ORRXrs,   AArch64::ORRXri,  AArch64::SDIVXr,  AArch64::UDIVXr,
+    AArch64::ADDXrx,   AArch64::ADDSXrs,  AArch64::ADDSXri, AArch64::ADDXrs,
+    AArch64::ADDXri,   AArch64::ANDSXri,  AArch64::ASRVXr,  AArch64::SUBXri,
+    AArch64::SUBXrs,   AArch64::SUBXrx,   AArch64::SUBSXrs, AArch64::SUBSXri,
+    AArch64::SBFMXri,  AArch64::CSELXr,   AArch64::ANDXri,  AArch64::ANDXrr,
+    AArch64::MADDXrrr, AArch64::MSUBXrrr, AArch64::EORXri,  AArch64::CSINVXr,
+    AArch64::CSINCXr,  AArch64::MOVZXi,   AArch64::MOVNXi,  AArch64::MOVKXi,
+    AArch64::LSLVXr,   AArch64::LSRVXr,   AArch64::ORNXrs,  AArch64::UBFMXri,
+    AArch64::BFMXri,   AArch64::ORRXrs,   AArch64::ORRXri,  AArch64::SDIVXr,
+    AArch64::UDIVXr,
 };
 
 bool has_s(int instr) {
@@ -1233,10 +1236,16 @@ public:
       store(*ret);
       break;
     }
+      // FIXME we currently don't support generation of the C flag in ANDS
+      //   This means we support the ANDri variant, but we don't support the
+      //   ANDrs variant.
+      // TODO: Is the carry flag set to 0 or carried through?
     case AArch64::ANDWri:
     case AArch64::ANDWrr:
     case AArch64::ANDXri:
-    case AArch64::ANDXrr: {
+    case AArch64::ANDXrr:
+    case AArch64::ANDSWri:
+    case AArch64::ANDSXri: {
       IR::Value *rhs;
       if (mc_inst.getOperand(2).isImm()) {
         auto imm = decodeLogicalImmediate(mc_inst.getOperand(2).getImm(), size);
@@ -1247,6 +1256,13 @@ public:
 
       auto and_op = add_instr<IR::BinOp>(*ty, move(next_name()), *get_value(1),
                                          *rhs, IR::BinOp::And);
+
+      // We don't set the C flag since it carries through.
+      if (has_s(opcode)) {
+        set_n(and_op);
+        set_z(and_op);
+      }
+
       store(*and_op);
       break;
     }
